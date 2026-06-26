8415f6fa8c12f53c5548fa630bb1486ebed4a2b3
huannguye <nguyenhuuhuan908@gmail.com>
2026-06-26 14:39:14 +0700

    feature/security-guard-shift-report-update-history-page

--- Views/HistoryWindow.xaml.cs
+++ Views/HistoryWindow.xaml.cs
@@ -13,5 +13,18 @@ namespace QuanLyGiuXe
             VM = new LichSuViewModel();
             DataContext = VM;
         }
+
+        private void DataGridRow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is System.Windows.Controls.DataGridRow row && row.Item is Models.LichSuXe)
+            {
+                var dialog = new Views.LichSuXeDetailWindow
+                {
+                    Owner = this,
+                    DataContext = this.DataContext
+                };
+                dialog.ShowDialog();
+            }
+        }
     }
 }
